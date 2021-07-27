b0c4cabfe8f585ebe9a09b0839382e571aa9895b
dttmm <vkdnjxodnd@naver.com>
2021-07-27 20:47:48 +0900

    가위바위보 버튼 클릭이벤트 등록

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import HandIcon from './HandIcon'
+import HandButton from './HandButton';
+import Button from './Button';
+
+function App() {
+  const handleClick = (value) => console.log(value);
+  const handleClearClick = () => console.log('처음부터');
+  return (
+    <div>
+      <Button onClick={handleClearClick}>처음부터</Button>
+      <HandButton value="rock" onClick={handleClick} />
+      <HandButton value="scissor" onClick={handleClick} />
+      <HandButton value="paper" onClick={handleClick} />
+    </div>
+  )
+}
+
+export default App
