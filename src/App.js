1e6a13b63970f146f00ee8c3c295c68f4cf68b00
dttmm <vkdnjxodnd@naver.com>
2021-07-28 18:59:05 +0900

    배점 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,48 +1,79 @@
-import { useState } from 'react';
-import Button from './Button';
-import HandButton from './HandButton';
-import HandIcon from './HandIcon';
-import { compareHand, generateRandomHand } from './utils';
+import { useState } from "react";
+import Button from "./Button";
+import HandButton from "./HandButton";
+import HandIcon from "./HandIcon";
+import { compareHand, generateRandomHand } from "./utils";
 
-const INITIAL_VALUE = 'rock';
+const INITIAL_VALUE = "rock";
 
 function getResult(me, other) {
   const comparison = compareHand(me, other);
-  if (comparison > 0) return '승리';
-  if (comparison < 0) return '패배';
-  return '무승부';
+  if (comparison > 0) return "승리";
+  if (comparison < 0) return "패배";
+  return "무승부";
 }
 
 function App() {
   const [hand, setHand] = useState(INITIAL_VALUE);
   const [otherHand, setOtherHand] = useState(INITIAL_VALUE);
   const [gameHistory, setGameHistory] = useState([]);
+  const [score, setScore] = useState(0);
+  const [otherScore, setOtherScore] = useState(0);
+  const [bet, setBet] = useState(1);
 
   const handleButtonClick = (nextHand) => {
     const nextOtherHand = generateRandomHand();
     const nextHistoryItem = getResult(nextHand, nextOtherHand);
+    const comparison = compareHand(nextHand, nextOtherHand);
     setHand(nextHand);
     setOtherHand(nextOtherHand);
-    // gameHistory에 nextHistoryItem 을 추가해주세요
     setGameHistory([...gameHistory, nextHistoryItem]);
+    if (comparison > 0) setScore(score + bet);
+    if (comparison < 0) setOtherScore(otherScore + bet);
   };
 
   const handleClearClick = () => {
     setHand(INITIAL_VALUE);
     setOtherHand(INITIAL_VALUE);
-    // gameHistory를 비워주세요
     setGameHistory([]);
+    setScore(0);
+    setOtherScore(0);
+    setBet(1);
+  };
+
+  const handleBetChange = (e) => {
+    // 여기에 코드를 작성하세요
+    // console.log(e.target.value)
+    let num = Number(e.target.value);
+    if (num > 9) {
+      num %= 10;
+    } else if (num < 1) {
+      num = 1;
+    }
+    setBet(num);
   };
 
   return (
     <div>
       <Button onClick={handleClearClick}>처음부터</Button>
       <div>
+        {score} : {otherScore}
+      </div>
+      <div>
         <HandIcon value={hand} />
         VS
         <HandIcon value={otherHand} />
       </div>
-      <p>승부 기록: {gameHistory.join(', ')}</p>
+      <div>
+        <input
+          onChange={handleBetChange}
+          type="number"
+          value={bet}
+          min={1}
+          max={9}
+        ></input>
+      </div>
+      <p>승부 기록: {gameHistory.join(", ")}</p>
       <div>
         <HandButton value="rock" onClick={handleButtonClick} />
         <HandButton value="scissor" onClick={handleButtonClick} />
