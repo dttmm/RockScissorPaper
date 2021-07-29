de263700be901d3a9fb2911441cd612d257198ca
dttmm <vkdnjxodnd@naver.com>
2021-07-29 11:39:34 +0900

    버튼 스타일 css로 적용

--- src/HandIcon.js
+++ src/HandIcon.js
@@ -1,16 +1,17 @@
-import rockImg from './assets/rock.svg';
-import scissorImg from './assets/scissor.svg';
-import paperImg from './assets/paper.svg';
+import rockImg from "./assets/rock.svg";
+import scissorImg from "./assets/scissor.svg";
+import paperImg from "./assets/paper.svg";
 
 const HAND_IMAGE = {
   rock: rockImg,
   scissor: scissorImg,
-  paper: paperImg
-}
+  paper: paperImg,
+};
 
-function HandIcon({ value = "rock" }) {
+// className prop을 추가하고, img 태그에 적용해주세요
+function HandIcon({ value, className = "" }) {
   const src = HAND_IMAGE[value];
-  return <img src={src} alt={value} />;
+  return <img className={className} src={src} alt={value} />;
 }
 
 export default HandIcon;
